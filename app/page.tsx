fb3f7d36c5261b37aa32c1216fdbe88a82756c5c
murathan aydin <murathan.aydin@epitech.eu>
2026-01-05 11:18:44 +0100

    remove testi

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             <ServicesSection />
             <ProjectsSection />
             <MethodSection />
-            <TestimonialsSection />
+            {/* <TestimonialsSection /> */}
             <SEOLocalSection />
             <FAQSection />
             <ContactSection />
